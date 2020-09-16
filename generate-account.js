e82b0df7e2857d3cf87384ccdbc2131b9d57a3ba
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2020-09-16 20:00:06 +0200

    Use merkle tree for public key

--- generate-account.js
+++ generate-account.js
@@ -1,56 +1,40 @@
 // Utility script to generate account details to put in genesis.json
 
 const bip39 = require('bip39');
-const SimpleLamport = require('simple-lamport');
+const ProperMerkle = require('proper-merkle');
 
-let lamport = new SimpleLamport();
+let merkle = new ProperMerkle();
 
 let mnemonic = bip39.generateMnemonic();
 
 console.log('MNEMONIC:', mnemonic);
 
-let seed = bip39.mnemonicToSeedSync(mnemonic).toString('hex')
+let network = 'lpos';
+let seed = bip39.mnemonicToSeedSync(mnemonic).toString('hex');
+let networkSeed = `${network}-${seed}`;
 
 console.log('SEED:', seed);
 
 console.log('');
 
-let candidacyKeys = lamport.generateKeysFromSeed(`${seed}-candidacy`, 0);
-let votingKeys = lamport.generateKeysFromSeed(`${seed}-voting`, 0);
-let forgingKeys = lamport.generateKeysFromSeed(`${seed}-forging`, 0);
-let multisigKeys = lamport.generateKeysFromSeed(`${seed}-multisig`, 0);
-let sigKeys = lamport.generateKeysFromSeed(`${seed}-sig`, 0);
+let candidacyTree = merkle.generateMSSTreeSync(`${networkSeed}-candidacy`, 0);
+let votingTree = merkle.generateMSSTreeSync(`${networkSeed}-voting`, 0);
+let forgingTree = merkle.generateMSSTreeSync(`${networkSeed}-forging`, 0);
+let multisigTree = merkle.generateMSSTreeSync(`${networkSeed}-multisig`, 0);
+let sigTree = merkle.generateMSSTreeSync(`${networkSeed}-sig`, 0);
 
-// console.log('------CANDIDACY------');
-// console.log(candidacyKeys);
-// console.log('------VOTING------');
-// console.log(votingKeys);
-// console.log('------FORGING------');
-// console.log(forgingKeys);
-// console.log('------MULTISIG------');
-// console.log(multisigKeys);
-// console.log('------SIG------');
-// console.log(sigKeys);
-// console.log('');
+let walletAddress = `${Buffer.from(sigTree.publicRootHash, 'base64').toString('hex')}lpos`;
 
-let candidacyPublicKeyHash = lamport.sha256(candidacyKeys.publicKey);
-let votingPublicKeyHash = lamport.sha256(votingKeys.publicKey);
-let forgingPublicKeyHash = lamport.sha256(forgingKeys.publicKey);
-let multisigPublicKeyHash = lamport.sha256(multisigKeys.publicKey);
-let sigPublicKeyHash = lamport.sha256(sigKeys.publicKey);
-
-let walletAddress = lamport.sha256(sigKeys.publicKey, 'hex');
-
-console.log('------CANDIDACY HASH------');
-console.log(candidacyPublicKeyHash);
-console.log('------VOTING HASH------');
-console.log(votingPublicKeyHash);
-console.log('------FORGING HASH------');
-console.log(forgingPublicKeyHash);
-console.log('------MULTISIG HASH------');
-console.log(multisigPublicKeyHash);
-console.log('------SIG HASH------');
-console.log(sigPublicKeyHash);
+console.log('------CANDIDACY PUBLIC KEY------');
+console.log(candidacyTree.publicRootHash);
+console.log('------VOTING PUBLIC KEY------');
+console.log(votingTree.publicRootHash);
+console.log('------FORGING PUBLIC KEY------');
+console.log(forgingTree.publicRootHash);
+console.log('------MULTISIG PUBLIC KEY------');
+console.log(multisigTree.publicRootHash);
+console.log('------SIG PUBLIC KEY------');
+console.log(sigTree.publicRootHash);
 
 console.log('');
 
